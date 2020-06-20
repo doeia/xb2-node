b6534607dd25b2fc9bea8f20f10c64d16f38d631
sanshi1929 <sanshi2929@gmail.com>
2020-06-20 17:44:51 +0900

    10structure

---
+++ src/post/post.service.ts
@@ -0,0 +1,15 @@
+export const getPosts = () => {
+  const data = [
+    {
+      content: '还让他和我',
+    },
+    {
+      content: '可预防人设实在',
+    },
+    {
+      content: '这三个号',
+    },
+  ];
+
+  return data;
+};
